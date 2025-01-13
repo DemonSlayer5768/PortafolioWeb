8fa3f7463a9f6be3a909f06eaf348a0bf1084dd5
jonathan <jjaimeloza5768@gmail.com>
2025-01-12 20:56:21 -0600

    Updates

---
+++ _astro/HeroSection.Ieocgi2Q.js
@@ -0,0 +1,315 @@
+import { G as u, j as e, F as g } from "./index.D-Byke-7.js";
+import { r } from "./index.0zUCkS4B.js";
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const f = (t) => t.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase(),
+  c = (...t) =>
+    t
+      .filter((a, l, n) => !!a && a.trim() !== "" && n.indexOf(a) === l)
+      .join(" ")
+      .trim();
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ var b = {
+  xmlns: "http://www.w3.org/2000/svg",
+  width: 24,
+  height: 24,
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  strokeWidth: 2,
+  strokeLinecap: "round",
+  strokeLinejoin: "round",
+};
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const j = r.forwardRef(
+  (
+    {
+      color: t = "currentColor",
+      size: a = 24,
+      strokeWidth: l = 2,
+      absoluteStrokeWidth: n,
+      className: o = "",
+      children: s,
+      iconNode: d,
+      ...x
+    },
+    m
+  ) =>
+    r.createElement(
+      "svg",
+      {
+        ref: m,
+        ...b,
+        width: a,
+        height: a,
+        stroke: t,
+        strokeWidth: n ? (Number(l) * 24) / Number(a) : l,
+        className: c("lucide", o),
+        ...x,
+      },
+      [
+        ...d.map(([h, p]) => r.createElement(h, p)),
+        ...(Array.isArray(s) ? s : [s]),
+      ]
+    )
+);
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const i = (t, a) => {
+  const l = r.forwardRef(({ className: n, ...o }, s) =>
+    r.createElement(j, {
+      ref: s,
+      iconNode: a,
+      className: c(`lucide-${f(t)}`, n),
+      ...o,
+    })
+  );
+  return (l.displayName = `${t}`), l;
+};
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const v = i("Code", [
+  ["polyline", { points: "16 18 22 12 16 6", key: "z7tu5w" }],
+  ["polyline", { points: "8 6 2 12 8 18", key: "1eg1df" }],
+]);
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const w = i("Laptop", [
+  [
+    "path",
+    {
+      d: "M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16",
+      key: "tarvll",
+    },
+  ],
+]);
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */ const y = i("Sparkles", [
+  [
+    "path",
+    {
+      d: "M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z",
+      key: "4pj2yx",
+    },
+  ],
+  ["path", { d: "M20 3v4", key: "1olli1" }],
+  ["path", { d: "M22 5h-4", key: "1gvqau" }],
+  ["path", { d: "M4 17v2", key: "vumght" }],
+  ["path", { d: "M5 18H3", key: "zchphs" }],
+]);
+function k(t) {
+  return u({
+    tag: "svg",
+    attr: { viewBox: "0 0 24 24" },
+    child: [
+      {
+        tag: "g",
+        attr: { id: "LinkedIn" },
+        child: [
+          {
+            tag: "g",
+            attr: {},
+            child: [
+              {
+                tag: "path",
+                attr: {
+                  d: "M18.44,3.06H5.56a2.507,2.507,0,0,0-2.5,2.5V18.44a2.507,2.507,0,0,0,2.5,2.5H18.44a2.5,2.5,0,0,0,2.5-2.5V5.56A2.5,2.5,0,0,0,18.44,3.06Zm1.5,15.38a1.511,1.511,0,0,1-1.5,1.5H5.56a1.511,1.511,0,0,1-1.5-1.5V5.56a1.511,1.511,0,0,1,1.5-1.5H18.44a1.511,1.511,0,0,1,1.5,1.5Z",
+                },
+                child: [],
+              },
+              {
+                tag: "g",
+                attr: {},
+                child: [
+                  {
+                    tag: "path",
+                    attr: {
+                      d: "M6.376,10.748a1,1,0,1,1,2,0v6.5h0a1,1,0,0,1-2,0Z",
+                    },
+                    child: [],
+                  },
+                  {
+                    tag: "circle",
+                    attr: { cx: "7.376", cy: "6.744", r: "1" },
+                    child: [],
+                  },
+                  {
+                    tag: "path",
+                    attr: {
+                      d: "M17.62,13.37v3.88a1,1,0,1,1-2,0V13.37a1.615,1.615,0,1,0-3.23,0v3.88a1,1,0,0,1-2,0v-6.5a1.016,1.016,0,0,1,1-1,.94.94,0,0,1,.84.47,3.609,3.609,0,0,1,5.39,3.15Z",
+                    },
+                    child: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  })(t);
+}
+const N = () => {
+    const n = `mailto:jjaimeloza5768@gmail.com?subject=${encodeURIComponent(
+      "Contacto desde la página web"
+    )}&body=${encodeURIComponent(
+      "Hola Jonathan, me gustaría ponerme en contacto contigo."
+    )}`;
+    window.location.href = n;
+  },
+  C = () => {
+    const t = document.createElement("a");
+    (t.href = "dist/files/JONATHAN-CV.pdf"),
+      (t.download = "CV_Jonathan.pdf"),
+      t.click();
+  },
+  A = "dist/images/FotoMe.jpg";
+//   dist\images
+
+function _() {
+  return e.jsx("div", {
+    className: "relative px-4 py-16 sm:px-6 sm:py-24 lg:px-8",
+    children: e.jsxs("div", {
+      className:
+        "flex flex-col lg:flex-row items-center justify-between gap-12",
+      children: [
+        e.jsxs("div", {
+          className: "text-center lg:text-left lg:w-1/2",
+          children: [
+            e.jsxs("h1", {
+              className: "text-4xl font-bold tracking-tight sm:text-4xl ",
+              children: [
+                e.jsx("span", {
+                  className: "block md:text-6xl",
+                  children: "Hola soy, Jonathan",
+                }),
+                e.jsx("span", {
+                  className: "block text-blue-400 md:text-5xl mt-3",
+                  children: "Software Developer",
+                }),
+              ],
+            }),
+            e.jsxs("p", {
+              className:
+                "mt-6 text-xl text-gray-800 dark:[&>strong]:text-blue-500 [&>strong]:text-blue-500 [&>strong]:font-semibold dark:text-gray-300 mb-5",
+              children: [
+                "Actulmente me encuentro cursando la carrera de",
+                " ",
+                e.jsx("strong", { children: "ingenieria en computacion" }),
+                " en el Centro Universitario de Ciencias Exactas e Ingenierías .",
+              ],
+            }),
+            e.jsxs("div", {
+              className:
+                "relative inline-flex overflow-hidden rounded-full p-[1px]",
+              children: [
+                e.jsxs("span", {
+                  className:
+                    "relative inline-flex overflow-hidden rounded-full p-[1px]",
+                  children: [
+                    e.jsx("span", {
+                      className:
+                        "absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#51A7E8_0%,#2154AE_50%,#51A7E8_100%)]",
+                    }),
+                    e.jsx("p", {
+                      className:
+                        "flex items-center justify-center w-full px-4 py-2 text-lg text-blue-800 bg-blue-100 rounded-full  dark:bg-gray-800 dark:text-white/80 backdrop-blur-3xl hover:bg-white hover:text-slate-900 transition-all",
+                      children: "Disponible para Trabajar",
+                    }),
+                  ],
+                }),
+                e.jsx("a", {
+                  className: "hover:text-blue-500 py-2 px-4 rounded-lg text-lg",
+                  href: "https://www.linkedin.com/in/jonathan-silvestre-jaime-loza-572795194/",
+                  target: "_blank",
+                  rel: "noopener noreferrer",
+                  children: e.jsx(k, { className: "text-4xl" }),
+                }),
+                e.jsx("a", {
+                  className: "hover:text-blue-500 py-2 px-4 rounded-lg text-lg",
+                  href: "https://github.com/DemonSlayer5768",
+                  target: "_blank",
+                  rel: "noopener noreferrer",
+                  children: e.jsx(g, { className: "text-4xl" }),
+                }),
+              ],
+            }),
+            e.jsxs("div", {
+              className:
+                "mt-8 flex flex-col sm:flex-row gap-4 justify-center lg:justify-start",
+              children: [
+                e.jsx("button", {
+                  className:
+                    "bg-blue-500  hover:bg-blue-600 py-2 px-4 rounded-lg text-lg ",
+                  onClick: C,
+                  children: "Descargar CV",
+                }),
+                e.jsx("button", {
+                  className:
+                    "border-2   hover:bg-white hover:text-slate-900 py-2 px-4 rounded-lg text-lg ",
+                  onClick: N,
+                  children: "Contactame",
+                }),
+              ],
+            }),
+          ],
+        }),
+        e.jsx("div", {
+          className: "lg:w-1/2 relative",
+          children: e.jsxs("div", {
+            className: "relative mx-auto w-64 h-64 sm:w-80 sm:h-80 lg:ml-40",
+            children: [
+              e.jsx("img", {
+                src: A,
+                alt: "JonaPhoto",
+                width: "350",
+                height: "350",
+                className: "rounded-full object-cover shadow-xl",
+              }),
+              e.jsx("div", {
+                className:
+                  "absolute -top-4 -right-4 w-24 h-24 bg-blue-500 rounded-full flex items-center justify-center animate-bounce",
+                children: e.jsx(v, { size: 40 }),
+              }),
+              e.jsx("div", {
+                className:
+                  "absolute -bottom-4 -left-4 w-20 h-20 bg-green-500 rounded-full flex items-center justify-center animate-pulse",
+                children: e.jsx(w, { size: 32 }),
+              }),
+              e.jsx("div", {
+                className:
+                  "absolute top-1/2 -translate-y-1/2 -left-8 w-16 h-16 bg-yellow-500 rounded-full flex items-center justify-center animate-spin-slow",
+                children: e.jsx(y, { size: 24 }),
+              }),
+            ],
+          }),
+        }),
+      ],
+    }),
+  });
+}
+export { _ as default };
---
+++ _astro/Projects.Cgg_6W91.js
@@ -0,0 +1,229 @@
+import { G as c, j as e, F as j, a as v } from "./index.D-Byke-7.js";
+import { r as i } from "./index.0zUCkS4B.js";
+function P(a) {
+  return c({
+    tag: "svg",
+    attr: { role: "img", viewBox: "0 0 24 24" },
+    child: [
+      {
+        tag: "path",
+        attr: {
+          d: "M18.665 21.978C16.758 23.255 14.465 24 12 24 5.377 24 0 18.623 0 12S5.377 0 12 0s12 5.377 12 12c0 3.583-1.574 6.801-4.067 9.001L9.219 7.2H7.2v9.596h1.615V9.251l9.85 12.727Zm-3.332-8.533 1.6 2.061V7.2h-1.6v6.245Z",
+        },
+        child: [],
+      },
+    ],
+  })(a);
+}
+function y(a) {
+  return c({
+    tag: "svg",
+    attr: { role: "img", viewBox: "0 0 24 24" },
+    child: [
+      {
+        tag: "path",
+        attr: {
+          d: "M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z",
+        },
+        child: [],
+      },
+    ],
+  })(a);
+}
+function N(a) {
+  return c({
+    tag: "svg",
+    attr: { viewBox: "0 0 512 512" },
+    child: [
+      {
+        tag: "path",
+        attr: {
+          d: "M160 389a20.91 20.91 0 0 1-13.82-5.2l-128-112a21 21 0 0 1 0-31.6l128-112a21 21 0 0 1 27.66 31.61L63.89 256l109.94 96.19A21 21 0 0 1 160 389zm192 0a21 21 0 0 1-13.84-36.81L448.11 256l-109.94-96.19a21 21 0 0 1 27.66-31.61l128 112a21 21 0 0 1 0 31.6l-128 112A20.89 20.89 0 0 1 352 389zm-144 48a21 21 0 0 1-20.12-27l96-320a21 21 0 1 1 40.23 12l-96 320A21 21 0 0 1 208 437z",
+        },
+        child: [],
+      },
+    ],
+  })(a);
+}
+function w(a) {
+  return c({
+    tag: "svg",
+    attr: { viewBox: "0 0 512 512" },
+    child: [
+      {
+        tag: "path",
+        attr: {
+          d: "M32 32v448h448V32zm240 348c0 43.61-25.76 64.87-63.05 64.87-33.68 0-53.23-17.44-63.15-38.49l34.28-20.75c6.61 11.73 11.63 21.65 26.06 21.65 12 0 21.86-5.41 21.86-26.46V240h44zm99.35 63.87c-39.09 0-64.35-17.64-76.68-42L329 382c9 14.74 20.75 24.56 41.5 24.56 17.44 0 27.57-7.72 27.57-19.75 0-14.43-10.43-19.54-29.68-28l-10.52-4.52c-30.38-12.92-50.52-29.16-50.52-63.45 0-31.57 24.05-54.63 61.64-54.63 26.77 0 46 8.32 59.85 32.68L396 290c-7.22-12.93-15-18-27.06-18-12.33 0-20.15 7.82-20.15 18 0 12.63 7.82 17.74 25.86 25.56l10.52 4.51c35.79 15.34 55.94 31 55.94 66.16.01 37.9-29.76 57.64-69.76 57.64z",
+        },
+        child: [],
+      },
+    ],
+  })(a);
+}
+const m = {
+    itemsImgsProyect1: [
+      { src: "dist//images/imgCineFlare1.jpg", alt: "img1" },
+      { src: "/dist/images/imgCineFlare2.jpg", alt: "img2" },
+      { src: "/dist/images/imgCineFlare3.jpg", alt: "img3" },
+      { src: "/dist/images/imgCineFlare4.jpg", alt: "img4" },
+    ],
+    itemsImgsProyect2: [
+      { src: "/dist/images/imgPDF2Ex1.jpg", alt: "imgP1" },
+      { src: "/dist/images/imgPDF2Ex2.jpg", alt: "imgP2" },
+      { src: "/dist/images/imgPDF2Ex3.jpg", alt: "imgP3" },
+      { src: "/dist/images/imgPDF2Ex4.jpg", alt: "imgP4" },
+      { src: "/dist/images/imgPDF2Ex5.jpg", alt: "imgP5" },
+    ],
+  },
+  r = {
+    NEXT: { name: "Next.js", class: "bg-[#4CAF50] text-white", icon: P },
+    TAILWIND: {
+      name: "Tailwind CSS",
+      class: "bg-[#1A73E8] text-white",
+      icon: y,
+    },
+    JAVASCRIPT: {
+      name: "JavaScript",
+      class: "bg-[#e6cc00] text-white",
+      icon: w,
+    },
+    PYTHON: { name: "Python", class: "bg-[#3776AB] text-white", icon: v },
+  },
+  d = [
+    {
+      title: "Pagina Web Streaming",
+      description:
+        "Proyecto de una página web de streaming de películas y series, con un diseño moderno y atractivo. Desarrollado con Next.js y Tailwind CSS.para el BackEnd se utilizo JavaScrip y para la base de datos use MySQL.",
+      github: "https://github.com/DemonSlayer5768/ProyectoWeb",
+      projectKey: "itemsImgsProyect1",
+      tags: [r.NEXT, r.TAILWIND, r.JAVASCRIPT],
+    },
+    {
+      title: "PDF2ExcelComparer",
+      description:
+        "El programa es una herramienta que extrae y compara datos de PDFs y Excel, ideal para contadores al validar estados de cuenta contra archivos auxiliares. Facilita la detección de discrepancias, incluso en PDFs protegidos, agilizando el proceso de verificación.",
+      github: "https://github.com/DemonSlayer5768/PDF2ExcelComparer",
+      projectKey: "itemsImgsProyect2",
+      tags: [r.PYTHON],
+    },
+  ];
+function C() {
+  const [a, x] = i.useState(0),
+    [g] = i.useState(d[0].projectKey),
+    o = m[g];
+  i.useEffect(() => {
+    const t = setInterval(() => {
+      p();
+    }, 4e3);
+    return () => clearInterval(t);
+  }, [a]);
+  const p = () => {
+    x((t) => (t === o.length - 1 ? 0 : t + 1));
+  };
+  return e.jsxs("div", {
+    className: "container mx-auto px-14 py-24",
+    children: [
+      e.jsxs("h1", {
+        className: "text-3xl font-bold text-left mb-12 flex items-center",
+        children: [e.jsx(N, { className: "mr-4 text-4xl" }), " Proyectos"],
+      }),
+      e.jsx("div", {
+        className: "flex flex-col gap-y-16",
+        children: d.map(
+          ({ title: t, description: h, tags: u, github: n, projectKey: f }) =>
+            e.jsxs(
+              "article",
+              {
+                className:
+                  "flex flex-col space-x-0 space-y-8 group md:flex-row md:space-x-8 md:space-y-0",
+                children: [
+                  e.jsx("div", {
+                    className: "relative w-full md:w-1/2",
+                    children: e.jsx("div", {
+                      className: "overflow-hidden rounded-xl shadow-lg",
+                      children: e.jsx("div", {
+                        className:
+                          "relative flex w-full h-64 transition-transform duration-700 ease-in-out transform",
+                        children: o.map((s, l) =>
+                          e.jsx(
+                            "img",
+                            {
+                              src: m[f][a].src,
+                              alt: s.alt,
+                              className: `absolute top-0 left-0 w-full h-full object-cover transform transition-transform duration-700  ${
+                                l === a
+                                  ? "translate-x-0"
+                                  : l > a
+                                  ? "translate-x-full"
+                                  : "-translate-x-full"
+                              }`,
+                            },
+                            s.alt
+                          )
+                        ),
+                      }),
+                    }),
+                  }),
+                  e.jsxs("div", {
+                    className: "w-full md:w-1/2 md:max-w-lg",
+                    children: [
+                      e.jsx("h3", {
+                        className: "text-2xl font-bold",
+                        children: t,
+                      }),
+                      e.jsxs("div", {
+                        className: "flex flex-wrap mt-2",
+                        children: [
+                          e.jsx("ul", {
+                            className: "flex flex-row mb-2 gap-x-2",
+                            children: u.map((s, l) =>
+                              e.jsx(
+                                "li",
+                                {
+                                  children: e.jsxs("span", {
+                                    className: `flex gap-x-2 rounded-full text-xs ${s.class} py-1 px-2`,
+                                    children: [
+                                      e.jsx(s.icon, { className: "text-lg" }),
+                                      s.name,
+                                    ],
+                                  }),
+                                },
+                                l
+                              )
+                            ),
+                          }),
+                          e.jsx("div", {
+                            className: "mt-3 text-justify",
+                            children: h,
+                          }),
+                          e.jsx("footer", {
+                            className:
+                              "flex items-end justify-start mt-4 gap-x-4",
+                            children:
+                              n &&
+                              e.jsxs("a", {
+                                href: n,
+                                target: "_blank",
+                                rel: "noopener noreferrer",
+                                className:
+                                  "flex items-center gap-x-2 px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700",
+                                children: [
+                                  e.jsx(j, { className: "text-lg" }),
+                                  "Code",
+                                ],
+                              }),
+                          }),
+                        ],
+                      }),
+                    ],
+                  }),
+                ],
+              },
+              t
+            )
+        ),
+      }),
+    ],
+  });
+}
+export { C as default };
