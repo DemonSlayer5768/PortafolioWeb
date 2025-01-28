f36f074888aff19cf9e59e9341293a39c13fc2f4
jonathan <jjaimeloza5768@gmail.com>
2025-01-28 00:06:42 -0600

    vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel(),
-
-  integrations: [react(), tailwind()],
+  site: "portafolio-il8u8ofy0-jonathans-projects-a35c48d3.vercel.app",
+  integrations: [
+    react(), // Integra React
+    tailwind(), // Integra Tailwind CSS
+    vercel(), // Integra Vercel para el despliegue
+  ],
 });
