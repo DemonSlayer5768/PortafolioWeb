60b7d8de7c015d81e4548a47236e3c895b798c90
jonathan <jjaimeloza5768@gmail.com>
2025-01-27 23:24:00 -0600

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://demonslayer5768.github.io",
-  base: "/",
+  output: "server", // Cambia esto si usas solo archivos estáticos.
+  adapter: vercel(),
 
   integrations: [react(), tailwind()],
 });
