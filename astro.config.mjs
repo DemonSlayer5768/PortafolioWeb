bb7a8f0ce704f23bc5abf9a23d54e988b757077b
jonathan <jjaimeloza5768@gmail.com>
2025-02-06 20:25:07 -0600

    cambios para deployd

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import vercelServerless from "@astrojs/vercel/serverless";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
